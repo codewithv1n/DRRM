819207c30a78f03c9a025d7a519b939fdc3d7f1b
codewithv1n <vinzelmarano@gmail.com>
2026-07-11 20:37:06 +0800

    feat: separation of concerns for api endpoints

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,16 @@
+// src/index.ts
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import testRoutes from './routes/dbtestRoutes';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use('/api/db-test', testRoutes);
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: "Server is Running" });
