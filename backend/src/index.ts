eda6dec097c5295561da4464f058fee5089432cd
codewithv1n <vinzelmarano@gmail.com>
2026-07-21 22:34:47 +0800

    feat: implementing api routes for controllers and also multer routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
-import testRoutes from './routes/dbtestRoutes';
+import path from 'path';
+import fs from 'fs';
+import residentRoutes from './routes/authRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
+const uploadsDir = path.join(__dirname, '../uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
 
-app.use('/api/db-test', testRoutes);
+app.use('/uploads', express.static(uploadsDir));
+app.use('/api/residents', residentRoutes);
+
+
 
 app.get('/', (req: Request, res: Response) => {
     res.json({ message: "Server is Running" });
