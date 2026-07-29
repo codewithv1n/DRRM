190224c39b91514adee0d91508c86d7a2a85f439
codewithv1n <vinzelmarano@gmail.com>
2026-07-29 21:25:30 +0800

    feat: adding new routes for frontend connections

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,35 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
 import UserLayout from './components/UserLayout'
+import StaffLayout from './components/StaffLayout'
 import AdminLayout from './components/AdminLayout'
-import HazardMapPage from './pages/users/HazardMapPage'
-import ReliefGoodsTrackerPage from './pages/users/ReliefGoodsTrackerPage'
-import IncidentReportPage from './pages/users/IncidentReportPage'
-import DisasterEarlyWarningPage from './pages/users/DisasterEarlyWarningPage'
-import DrrmCoordinationPage from './pages/users/DrrmCoordinationPage'
-import LoginPage from './auth/users/LoginPage'
-import SignupPage from './auth/users/SignupPage'
-import AdminLoginPage from './auth/admin/adminLoginPage'
-import AdminSignupPage from './auth/admin/adminSignupPage'
+import HazardMapPage from './pages/resident/HazardMapPage'
+import ReliefGoodsTrackerPage from './pages/resident/ReliefGoodsTrackerPage'
+import IncidentReportPage from './pages/resident/IncidentReportPage'
+import DisasterEarlyWarningPage from './pages/resident/DisasterEarlyWarningPage'
+import DrrmCoordinationPage from './pages/resident/DrrmCoordinationPage'
+import LoginPage from './auth/LoginPage'
+import SignupPage from './auth/SignupPage'
 
-// Admin Pages
-import DashboardPage from './pages/admin/DashboardPage'
-import IncidentManagementPage from './pages/admin/IncidentManagementPage'
-import ReliefInventoryPage from './pages/admin/ReliefInventoryPage'
-import MapControlPage from './pages/admin/MapControlPage'
-import AlertsContentPage from './pages/admin/AlertsContentPage'
-import RecordsPage from './pages/admin/RecordsPage'
+// Staff Pages
+import DashboardPage from './pages/staff/DashboardPage'
+import IncidentManagementPage from './pages/staff/IncidentManagementPage'
+import ReliefInventoryPage from './pages/staff/ReliefInventoryPage'
+import MapControlPage from './pages/staff/EvacuationControlPage'
+import AlertsContentPage from './pages/staff/AlertsContentPage'
+import DrrmManagementPage from './pages/staff/DrrmManagementPage'
+
+// Admin Page
+import AccountManagementPage from './pages/admin/AccountManagementPage'
+
+// Responder Pages
+import ResponderLayout from './components/ResponderLayout'
+import ResponderDashboardPage from './pages/responder/ResponderDashboardPage'
 
 function App() {
   return (
     <Routes>
       <Route element={<UserLayout />}>
-        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/" element={<Navigate to="/hazard-evac" replace />} />
         <Route path="/hazard-evac" element={<HazardMapPage />} />
         <Route path="/relief-goods" element={<ReliefGoodsTrackerPage />} />
         <Route path="/incident-report" element={<IncidentReportPage />} />
@@ -31,20 +37,26 @@ function App() {
         <Route path="/coordination" element={<DrrmCoordinationPage />} />
       </Route>
 
+      <Route element={<StaffLayout />}>
+        <Route path="/staff" element={<DashboardPage />} />
+        <Route path="/staff/incidents" element={<IncidentManagementPage />} />
+        <Route path="/staff/relief" element={<ReliefInventoryPage />} />
+        <Route path="/staff/evac-center" element={<MapControlPage />} />
+        <Route path="/staff/drrm" element={<DrrmManagementPage />} />
+        <Route path="/staff/alerts" element={<AlertsContentPage />} />
+      </Route>
+
       <Route element={<AdminLayout />}>
         <Route path="/admin" element={<DashboardPage />} />
-        <Route path="/admin/incidents" element={<IncidentManagementPage />} />
-        <Route path="/admin/relief" element={<ReliefInventoryPage />} />
-        <Route path="/admin/map" element={<MapControlPage />} />
-        <Route path="/admin/alerts" element={<AlertsContentPage />} />
-        <Route path="/admin/records" element={<RecordsPage />} />
+        <Route path="/admin/accounts" element={<AccountManagementPage />} />
+      </Route>
+
+      <Route element={<ResponderLayout />}>
+        <Route path="/responder" element={<ResponderDashboardPage />} />
       </Route>
 
       <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage/>} />
-
-      <Route path="/adminLogin" element={<AdminLoginPage />}/>
-       <Route path="/adminSignup" element={<AdminSignupPage />}/>
+      <Route path="/signup" element={<SignupPage />} />
     </Routes>
   )
 }
